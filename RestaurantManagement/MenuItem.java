d542d74c8c061f83d58a5a91f7f34f0eff26fb5e
AdrianJoseph99 <57686487+AdrianJoseph99@users.noreply.github.com>
2019-11-20 14:14:21 -0500

    Add files via upload

---
+++ RestaurantManagement/MenuItem.java
@@ -0,0 +1,46 @@
+/**
+ * Write a description of class MenuItem here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MenuItem
+{
+    protected double price;
+    protected String name;
+    /**
+     * Constructor for objects of class MenuItem
+     */
+    public MenuItem(String name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+
+        return price;
+    }
+
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+}
