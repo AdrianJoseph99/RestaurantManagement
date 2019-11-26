3cbac6dfad304a9b604658c23da3dd177090ec14
AaronKim10 <57686399+AaronKim10@users.noreply.github.com>
2019-11-25 19:53:43 -0500

    Order Class

---
+++ Order.java
@@ -0,0 +1,54 @@
+import java.util.Calendar;
+import java.util.*;
+
+public class Order {
+ 
+  protected int tableNumber;
+  protected ArrayList<MenuItem> order;
+  protected Calendar timeOfOrder;
+  
+  public Order(int tableNumber, ArrayList<MenuItem> order, Calendar timeOfOrder) {
+    this.tableNumber = tableNumber;
+    this.order = order;
+    this.timeOfOrder = Calendar.getInstance();
+  }
+  
+  public Order() {
+  }
+    
+  
+  public void setTimeOfOrder(Calendar timeOfOrder) {
+    this.timeOfOrder = timeOfOrder;
+  }
+  public void setTableNumber(int tableNumber) {
+    this.tableNumber = tableNumber;
+  }
+  public void setOrder(ArrayList<MenuItem> order) {
+    this.order = order;
+  }
+  public Calendar getTimeOfOrder() {
+    return this.timeOfOrder;
+  }
+  public int getTableNumber() {
+    return this.tableNumber;
+  }
+  public ArrayList<MenuItem> getOrder() {
+    return this.order;
+  }
+  
+  public double calculateTotal(ArrayList<MenuItem> order) {
+    double total = 0;
+    for (MenuItem item : order) {
+      total += item.getPrice();
+    }
+    return total;
+  }
+ 
+  public void orderTime() {
+    System.out.println("Time of Order:"); 
+    System.out.format("%tB %te, %tY%n", this.timeOfOrder, this.timeOfOrder, this.timeOfOrder);
+    System.out.format("%tl:%tM %tp%n", this.timeOfOrder, this.timeOfOrder, this.timeOfOrder); 
+  }
+
+}
+  
